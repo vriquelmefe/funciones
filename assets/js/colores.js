dc2831b19ffc825bca1c351414ab2ec090f141d7
vriquelmefe <vriquelmefe@gmail.com>
2024-06-14 20:31:56 -0400

    fix ejercicio caja colores

--- assets/js/colores.js
+++ assets/js/colores.js
@@ -1,5 +1,9 @@
 
-function pintar(arg) {
-let elemento = document.querySelector(arg);
- elemento.style.background = 'black';
+function pintar(event) {
+    let elemento = event.target;
+    elemento.style.background = 'black';
 }
+document.getElementById('div1').addEventListener('click', pintar);
+document.getElementById('div2').addEventListener('click', pintar);
+document.getElementById('div3').addEventListener('click', pintar);
+document.getElementById('div4').addEventListener('click', pintar);
